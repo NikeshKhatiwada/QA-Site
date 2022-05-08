4b6881d68de2e55f4b45c85b06cf400039f26b08
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-08 12:47:02 +0545

    Created home page and navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,60 @@
+<nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="/index">Home</a>
+            <a class="navbar-item" href="/tags">Tags</a>
+            <a class="navbar-item" href="/tags">Users</a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    More
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="/about">
+                        About
+                    </a>
+                    <a class="navbar-item" href="/contact">
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-ghost" href="/search">
+                        <i class="gg-search"></i>
+                    </a>
+
+                    <a class="button is-ghost" href="/notifications">
+                        <i class="gg-bell"></i>
+                    </a>
+
+                    <a class="button is-ghost" href="/profile">
+                        <i class="gg-profile"></i>
+                    </a>
+
+                    <form method="post" action="/logout">
+                        <button type="submit" class="button is-ghost">
+                            <i class="gg-log-out"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
