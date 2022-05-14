19a090b81cecaba75b06f25c1577b599809a3b6a
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-14 17:06:34 +0545

    Added code to handle user sessions

--- app/Http/Controllers/UserSessionsController.php
+++ app/Http/Controllers/UserSessionsController.php
@@ -9,4 +9,23 @@ class UserSessionsController extends Controller
     public function create() {
         return view('users.sessions.create');
     }
+
+    public function store() {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password'=> ['required']
+        ]);
+
+        if(! auth('web')->attempt($attributes)) {
+            return back()->withInput()->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        }
+
+        session()->regenerate();
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
+    public function destroy() {
+        auth('web')->logout();
+        return redirect('/')->with('success', 'Goodbye!');
+    }
 }
