23915aa7da2201d156ca5bafa216ac6b6962b5af
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-18 20:04:03 +0545

    Added code in QuestionController to show questions in index page

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function index() {
+        $questions = Question::all();
+        $questions->map(function ($question) {
+            $question->top_answer = $question->answers->sortByDesc(function ($answer) {
+                return $answer->answerVotes()->sum('vote');
+            })->first();
+            if($question->top_answer) {
+                $question->top_answer->upvotes = $question->top_answer->answerVotes()->where('vote', 1)->count();
+                $question->top_answer->downvotes = $question->top_answer->answerVotes()->where('vote', -1)->count();
+                $question->top_answer->vote = $question->top_answer->answerVotes()->where('user_id', auth('web')->id())->value('vote');
+            }
+        });
+        if(request()->has('question_type'))
+        {
+            if(request('question_type') === 'new') {
+                return view('index', [
+                    'questions' => $questions->sortByDesc('created_at')
+                ]);
+            }
+            if(request('question_type') === 'top') {
+                return view('index', [
+                    'questions' => $questions->sortByDesc(function ($question) {
+                        return $question->questionVotes()->sum('vote');
+                    })
+                ]);
+            }
+        }
+        return view('index', [
+            'questions' => $questions->sortBy('slug')
+        ]);
+    }
 }
