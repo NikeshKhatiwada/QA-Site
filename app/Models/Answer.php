57c5f5e16aa4c5d15531d84f8d1545df7aabf24d
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-19 17:51:46 +0545

    Added timestamps for Answer relationship with Votes in Answer model

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -33,7 +33,7 @@ class Answer extends Model
     }
 
     public function answerVotes() {
-        return $this->belongsToMany(User::class, 'users_vote_answers', 'answer_id', 'user_id');
+        return $this->belongsToMany(User::class, 'users_vote_answers', 'answer_id', 'user_id')->withTimestamps();
     }
 
     public function answerReports() {
