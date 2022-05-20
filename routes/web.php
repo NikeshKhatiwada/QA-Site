7b04d2a8a924ef1dce9fec4f80c9f466713b70d0
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-20 10:30:25 +0545

    Added routes for questions, answers and users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserSessionsController;
 use Illuminate\Support\Facades\Route;
@@ -15,18 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});//->middleware('auth:web');
-
-Route::get('/home', function () {
-    return view('index');
-});//->middleware('auth:web');
-
-Route::get('/question/show', function () {
-    return view('questions.show');
-});
-
 Route::get('/question/create', function () {
     return view('questions.create');
 }); //->middleware('user');
@@ -35,13 +27,52 @@ Route::get('/question/edit', function () {
     return view('questions.edit');
 }); //->middleware('user');
 
-Route::get('/tags', [TagController::class, 'index']);
-Route::get('/tag/show/{tag}', [TagController::class, 'show']);
-Route::get('/tag/create', [TagController::class, 'create']);
-Route::get('/tag/edit', [TagController::class, 'edit']);
+Route::middleware('auth:web')->group(function () {
+    Route::controller(QuestionController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/home', 'index');
+        Route::get('/question/{question:slug}/show', 'show');
+        Route::get('/questions/create', 'create');
+        Route::post('/questions', 'store');
+        Route::get('/question/{question:slug}/edit', 'edit');
+        Route::patch('/question/{question:slug}', 'update');
+        Route::post('/question/follow', 'follow');
+        Route::post('/question/unfollow', 'unfollow');
+        Route::post('/question/upvote', 'upvote');
+        Route::post('/question/downvote', 'downvote');
+        Route::delete('/question/{question:slug}', 'destroy');
+    });
 
-Route::get('/users', function () {
-    return view('users.index');
+    Route::controller(AnswerController::class)->group(function () {
+        Route::post('/answer/upvote', 'upvote');
+        Route::post('/answer/downvote', 'downvote');
+    });
+
+    Route::controller(TagController::class)->group(function () {
+        Route::get('/tags', 'index');
+        Route::get('/tag/{tag:slug}/show', 'show');
+        Route::get('/tag/create', 'create');
+        Route::get('/tag/edit', 'edit');
+        Route::post('/tag/follow', 'follow');
+        Route::post('/tag/unfollow', 'unfollow');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/user/{user:username}/show', 'show');
+        Route::post('/user/follow', 'follow');
+        Route::post('/user/unfollow', 'unfollow');
+    });
+    Route::controller(UserProfileController::class)->group(function() {
+        Route::get('/profile/show', 'show');
+        Route::get('/profile/edit', 'edit');
+        Route::get('/profile/edit-password', 'editPassword');
+        Route::patch('/profile', 'update');
+        Route::patch('/profile/password', 'updatePassword');
+        Route::delete('/profile', 'delete');
+    });
 });
 
 Route::get('/login', [UserSessionsController::class, 'create'])->name('login')->middleware('guest');
+Route::post('sessions', [UserSessionsController::class, 'store'])->middleware('guest');
+Route::get('/register', [UserProfileController::class, 'create'])->name('register')->middleware('guest');
