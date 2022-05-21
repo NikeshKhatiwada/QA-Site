40a99f526655a6b1371429fb6f9f9f03c25897c9
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-21 20:40:51 +0545

    Added code in Question Controller to store Views of Questions and also display Suggestions based on Views

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -40,8 +40,72 @@ class QuestionController extends Controller
                 ]);
             }
         }
+        $tags = Tag::all()->sortBy('id');
+        $user_id = auth('web')->id();
+        $viewed_questions = $questions->filter(function ($question) use ($user_id) {
+            return ($question->questionViews()->where('user_id', $user_id)->exists());
+        });
+        $questions = $questions->diffAssoc($viewed_questions);
+        $viewed_questions->map(function ($viewed_question) {
+            $viewed_question->points = [];
+        });
+        $questions->map(function ($question) {
+            $question->points = [];
+            $question->distance = 0;
+        });
+        $viewed_questions->map(function ($viewed_question) use ($tags) {
+            foreach ($tags as $tag) {
+                if ($viewed_question->tags->contains('id', $tag->id)) {
+                    $viewed_question->points = array_merge($viewed_question->points, [1]);
+                } else {
+                    $viewed_question->points = array_merge($viewed_question->points, [0]);
+                }
+            }
+        });
+        $questions->map(function ($question) use ($tags) {
+            foreach ($tags as $tag) {
+                if($question->tags->contains('id', $tag->id)) {
+                    $question->points = array_merge($question->points, [1]);
+                }
+                else {
+                    $question->points = array_merge($question->points, [0]);
+                }
+            }
+        });
+        $suggested_questions = collect();
+        $viewed_questions_count = $viewed_questions->count();
+        $viewed_questions->map(function ($viewed_question) use ($viewed_questions_count, $suggested_questions, $questions) {
+            $questions->map(function ($question) use ($viewed_question) {
+                $question->distance = 0;
+                $pointsDifference = [];
+                $i = 0;
+                while ($i < count($viewed_question->points) - 10) {
+                    $pointsDifference[$i] = pow(($viewed_question->points[$i] - $question->points[$i]), 2);
+                    $i++;
+                }
+                $question->distance = (int)(sqrt(array_sum($pointsDifference)));
+            });
+            $questions->sortBy(function ($question) {
+                return $question->distance;
+            });
+            $questions = $questions->take($viewed_questions_count);
+            $questions->map(function ($question) use ($suggested_questions) {
+                $suggested_questions->push($question);
+            });
+        });
+        $suggested_questions = $suggested_questions->unique();
+        $suggested_questions->map(function ($question) {
+            $question->top_answer = $question->answers->sortByDesc(function ($answer) {
+                return $answer->answerVotes()->sum('vote');
+            })->first();
+            if($question->top_answer) {
+                $question->top_answer->upvotes = $question->top_answer->answerVotes()->where('vote', 1)->count();
+                $question->top_answer->downvotes = $question->top_answer->answerVotes()->where('vote', -1)->count();
+                $question->top_answer->vote = $question->top_answer->answerVotes()->where('user_id', auth('web')->id())->value('vote');
+            }
+        });
         return view('index', [
-            'questions' => $questions->sortBy('slug')
+            'questions' => $suggested_questions
         ]);
     }
 
@@ -58,8 +122,16 @@ class QuestionController extends Controller
             $answer->vote = $answer->answerVotes()->where('user_id', auth('web')->id())->value('vote');
         });
         $question->answer = $question->answers->where('user_id', auth('web')->id())->first();
+        $attributes['question_id'] = $question->id;
+        $attributes['user_id'] = auth('web')->id();
+        $attributes['views'] = 1;
+        if($question->questionViews()->where('user_id', auth('web')->id())->value('views') !== null) {
+            $attributes['views'] = $question->questionViews()->where('user_id', auth('web')->id())->value('views') + 1;
+            $question->questionViews()->detach($attributes['user_id']);
+        }
+        $question->questionViews()->attach([$attributes]);
         return view('questions.show', [
-            'question' => $question,
+            'question' => $question
         ]);
     }
 
