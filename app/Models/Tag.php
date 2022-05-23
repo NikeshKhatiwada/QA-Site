889aeffabef776b6c06f4da0dfff7bdb8cde558d
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-05-23 16:41:58 +0545

    Added function for Tag Category and Tag Reports in Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,11 +10,20 @@ class Tag extends Model
     use HasFactory;
 
     protected $fillable = [
+        'tag_category_id',
         'slug',
         'title',
         'description',
         'image',
     ];
+
+    protected $with = [
+        'tag_category'
+    ];
+
+    public function tagCategory() {
+        return $this->belongsTo(Tag::class);
+    }
 
     public function questions() {
         return $this->belongsToMany(Question::class, 'questions_tags', 'tag_id', 'question_id');
@@ -22,5 +31,9 @@ class Tag extends Model
 
     public function tagFollowers() {
         return $this->belongsToMany(User::class, 'users_follow_tags', 'tag_id', 'user_id')->withTimestamps();
+    }
+
+    public function tagReports() {
+        return $this->hasMany(Report::class, 'report_about_id')->where('report_about_category', '=', 10);
     }
 }
