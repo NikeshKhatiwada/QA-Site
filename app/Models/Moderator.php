893f28ab6c5816615b964f8a400e742e26409dec
NikeshKhatiwada <nikeshkhatiwada@outlook.com>
2022-06-27 17:23:30 +0545

    Modified code in Moderator model class to allow authentication

--- app/Models/Moderator.php
+++ app/Models/Moderator.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Moderator extends Model
+class Moderator extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'username',
         'first_name',
